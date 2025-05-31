95922a69cd936d812d736ea37b669a13f5453e2f
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-05-31 23:23:13 +0530

    heroSection Fix

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client"
+
+import useSelected from "@/hooks/useSelected"
+
 
 const HeroSection = () => {
+
+  const { selected } = useSelected()
+
+
   return (
-    <section className='justify-center pt-12 pb-8'>
+    <section className={`justify-center pt-12 pb-8 relative ${selected ? 'hidden' : 'flex'}`}>
         <div className='flex flex-col gap-4'>
-            <h1>
-                <span>Generate memes in seconds <span>Seconds</span></span>
+            <h1 className="text-center">
+                <span>Generate memes in <span>Seconds</span></span>
             </h1>
             <p className='text-lg md:text-xl text-muted-foreground text-center'>
                 without dealing with a messy UI
