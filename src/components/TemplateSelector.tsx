50689aa001ce33eda434c60c5d3739bbd1bdf721
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-06-03 17:18:44 +0530

    template seletor.tsx

--- src/components/TemplateSelector.tsx
+++ src/components/TemplateSelector.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Template } from '@/types/templates';
 
 import { motion, AnimatePresence } from 'framer-motion';
+import Image from 'next/image';
 
 type TemplateSelectorProps = {
     templates: Record<string, Template>;
@@ -12,9 +13,44 @@ type TemplateSelectorProps = {
 
 export default function TemplateSelector({ templates, onSelect }: TemplateSelectorProps) {
     return (
-        <section>
+        <section className='grid grid-cols-3 max-sm:grid-cols-1 gap-6 grid-flow-dense'>
+            <AnimatePresence mode="popLayout">
 
-            
+                {Object.entries(templates).map(([key, tpl], index) => (
+                    <motion.div
+                        key={key}
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -20 }}
+                        transition={{
+                            duration: 0.3,
+                            delay: index * 0.05,
+                            ease: [0.22, 1, 0.36, 1]
+                        }}
+
+                        onClick={() => onSelect(key)}
+                    >
+                        {/* change div to motion.div */}
+                        <div>
+                            <Image
+                                src={tpl.image}
+                                alt={key}
+                                fill
+                                className=" object-cover rounded-2xl shadow"
+                                loading='lazy'
+                            />
+
+                        </div>
+                        <p>
+                            {key.replace(/-/g, " ")}
+                        </p>
+
+                    </motion.div>
+
+                ))}
+            </AnimatePresence>
+
+
         </section>
     );
 }
